ee0e4f9e1dec91fa20133f0e7e522d9a489aec76
lfq7413 <lfq7413@gmail.com>
2016-02-29 20:31:16 +0800

    添加配置文件

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import "github.com/astaxie/beego"
+
+// Config ...
+type Config struct {
+	URL string
+}
+
+var (
+    // TConfig ...
+	TConfig *Config
+)
+
+func init() {
+	TConfig = &Config{
+		URL: "http://127.0.0.1:8080/v1/",
+	}
+
+	ParseConfig()
+}
+
+// ParseConfig ...
+func ParseConfig() {
+	TConfig.URL = beego.AppConfig.String("myhttpurl")
+}

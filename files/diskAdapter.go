1d01c350f4a15eec84cae86a85b6b6a92886de29
lfq7413 <lfq7413@gmail.com>
2016-03-24 17:32:40 +0800

    完成 files

--- files/diskAdapter.go
+++ files/diskAdapter.go
@@ -3,12 +3,12 @@ package files
 type diskAdapter struct {
 }
 
-func (d *diskAdapter) createFile(filename string, data []byte, contentType string) {
-
+func (d *diskAdapter) createFile(filename string, data []byte, contentType string) error {
+	return nil
 }
 
-func (d *diskAdapter) deleteFile(filename string) {
-
+func (d *diskAdapter) deleteFile(filename string) error {
+	return nil
 }
 func (d *diskAdapter) getFileData(filename string) []byte {
 	return nil
--- files/files.go
+++ files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import "github.com/lfq7413/tomato/config"
+import "github.com/lfq7413/tomato/utils"
 
 var adapter filesAdapter
 
@@ -20,24 +21,62 @@ func GetFileData(filename string) []byte {
 
 // CreateFile ...
 func CreateFile(filename string, data []byte, contentType string) map[string]string {
-	adapter.createFile(filename, data, contentType)
-	return nil
+	extname := utils.ExtName(filename)
+	if extname == "" && contentType != "" && utils.LookupExtension(contentType) != "" {
+		filename = filename + "." + utils.LookupExtension(contentType)
+	} else if extname != "" && contentType == "" {
+		contentType = utils.LookupContentType(filename)
+	}
+
+	filename = utils.CreateToken() + "-" + filename
+	location := adapter.getFileLocation(filename)
+
+	err := adapter.createFile(filename, data, contentType)
+
+	if err != nil {
+		return nil
+	}
+	return map[string]string{
+		"url":  location,
+		"name": filename,
+	}
 }
 
 // DeleteFile ...
 func DeleteFile(filename string) error {
-	adapter.deleteFile(filename)
-	return nil
+	return adapter.deleteFile(filename)
 }
 
 // ExpandFilesInObject ...
 func ExpandFilesInObject(object interface{}) {
+	if utils.SliceInterface(object) != nil {
+		objs := utils.SliceInterface(object)
+		for _, obj := range objs {
+			ExpandFilesInObject(obj)
+		}
+	}
 
+	if utils.MapInterface(object) == nil {
+		return
+	}
+
+	obj := utils.MapInterface(object)
+
+	for _, v := range obj {
+		fileObject := utils.MapInterface(v)
+		if fileObject != nil && fileObject["__type"] == "File" {
+			if fileObject["url"] != nil {
+				continue
+			}
+			filename := utils.String(fileObject["name"])
+			fileObject["url"] = adapter.getFileLocation(filename)
+		}
+	}
 }
 
 type filesAdapter interface {
-	createFile(filename string, data []byte, contentType string)
-	deleteFile(filename string)
+	createFile(filename string, data []byte, contentType string) error
+	deleteFile(filename string) error
 	getFileData(filename string) []byte
 	getFileLocation(filename string) string
 }

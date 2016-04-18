5c4099426190a299c714191123c36f1e383fbe53
lfq7413 <lfq7413@gmail.com>
2016-04-18 21:30:47 +0800

    重构 files.go

--- controllers/files.go
+++ controllers/files.go
@@ -1,16 +1,18 @@
 package controllers
 
 import (
+	"github.com/lfq7413/tomato/errs"
 	"github.com/lfq7413/tomato/files"
 	"github.com/lfq7413/tomato/utils"
 )
 
-// FilesController ...
+// FilesController 处理 /files 接口的请求
+// TODO 可能不需要权限验证
 type FilesController struct {
 	ObjectsController
 }
 
-// HandleGet ...
+// HandleGet 处理下载文件请求
 // @router /:appId/:filename [get]
 func (f *FilesController) HandleGet() {
 	filename := f.Ctx.Input.Param(":filename")
@@ -27,46 +29,52 @@ func (f *FilesController) HandleGet() {
 	}
 }
 
-// HandleCreate ...
+// HandleCreate 处理上传文件请求
 // @router /:filename [post]
 func (f *FilesController) HandleCreate() {
 	filename := f.Ctx.Input.Param(":filename")
 	data := f.Ctx.Input.RequestBody
 	if data == nil && len(data) == 0 {
-		// TODO 无效上传
+		f.Data["json"] = errs.ErrorMessageToMap(errs.FileSaveError, "Invalid file upload.")
+		f.ServeJSON()
 		return
 	}
 	if len(filename) > 128 {
-		// TODO 文件名太长
+		f.Data["json"] = errs.ErrorMessageToMap(errs.InvalidFileName, "Filename too long.")
+		f.ServeJSON()
 		return
 	}
 	if utils.IsFileName(filename) == false {
-		// TODO 无效文件名
+		f.Data["json"] = errs.ErrorMessageToMap(errs.InvalidFileName, "Filename contains invalid characters.")
+		f.ServeJSON()
 		return
 	}
 	contentType := f.Ctx.Input.Header("Content-type")
 	result := files.CreateFile(filename, data, contentType)
 	if result != nil && result["url"] != "" {
 		f.Ctx.Output.SetStatus(201)
-		f.Ctx.Output.Header("Location", result["url"])
+		f.Ctx.Output.Header("location", result["url"])
 		f.Data["json"] = result
 		f.ServeJSON()
 	} else {
-		// TODO 保存文件失败
+		f.Data["json"] = errs.ErrorMessageToMap(errs.FileSaveError, "Could not store file.")
+		f.ServeJSON()
 	}
 }
 
-// HandleDelete ...
+// HandleDelete 处理删除文件请求
 // @router /:filename [delete]
 func (f *FilesController) HandleDelete() {
 	if f.Auth.IsMaster == false {
-		// TODO 权限不足
+		f.Data["json"] = errs.ErrorMessageToMap(errs.FileDeleteError, "This user is not allowed to delete file.")
+		f.ServeJSON()
 		return
 	}
 	filename := f.Ctx.Input.Param(":filename")
 	err := files.DeleteFile(filename)
 	if err != nil {
-		// TODO 删除失败
+		f.Data["json"] = errs.ErrorMessageToMap(errs.FileDeleteError, "Could not delete file.")
+		f.ServeJSON()
 		return
 	}
 	f.Data["json"] = "{}"

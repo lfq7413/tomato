cb31902785de19fe2049024fa90d97d20e9a72d4
lfq7413 <lfq7413@gmail.com>
2016-04-20 16:40:23 +0800

    重构 installations.go

--- controllers/installations.go
+++ controllers/installations.go
@@ -1,18 +1,18 @@
 package controllers
 
-// InstallationsController ...
+// InstallationsController 处理 /installations 接口的请求
 type InstallationsController struct {
 	ObjectsController
 }
 
-// HandleFind ...
+// HandleFind 处理查找已安装设备请求
 // @router / [get]
 func (i *InstallationsController) HandleFind() {
 	i.ClassName = "_Installation"
 	i.ObjectsController.HandleFind()
 }
 
-// HandleGet ...
+// HandleGet 处理获取指定设备信息请求
 // @router /:objectId [get]
 func (i *InstallationsController) HandleGet() {
 	i.ClassName = "_Installation"
@@ -20,14 +20,14 @@ func (i *InstallationsController) HandleGet() {
 	i.ObjectsController.HandleGet()
 }
 
-// HandleCreate ...
+// HandleCreate 处理添加设备请求
 // @router / [post]
 func (i *InstallationsController) HandleCreate() {
 	i.ClassName = "_Installation"
 	i.ObjectsController.HandleCreate()
 }
 
-// HandleUpdate ...
+// HandleUpdate 处理更新设备信息请求
 // @router /:objectId [put]
 func (i *InstallationsController) HandleUpdate() {
 	i.ClassName = "_Installation"
@@ -35,7 +35,7 @@ func (i *InstallationsController) HandleUpdate() {
 	i.ObjectsController.HandleUpdate()
 }
 
-// HandleDelete ...
+// HandleDelete 处理删除指定设备请求
 // @router /:objectId [delete]
 func (i *InstallationsController) HandleDelete() {
 	i.ClassName = "_Installation"

68d93ff128a8d5e673ed9b55a321f8906bc36a37
lfq7413 <lfq7413@gmail.com>
2016-04-22 23:13:18 +0800

    重构 rest.go

--- rest/rest.go
+++ rest/rest.go
@@ -1,11 +1,19 @@
 package rest
 
 import (
+	"github.com/lfq7413/tomato/errs"
 	"github.com/lfq7413/tomato/types"
 	"github.com/lfq7413/tomato/utils"
 )
 
-// Find ...
+// Find 根据条件查找数据
+// 返回格式如下：
+// {
+// 	"results":[
+// 		{...},
+// 	],
+// 	"count":10
+// }
 func Find(
 	auth *Auth,
 	className string,
@@ -13,13 +21,16 @@ func Find(
 	options types.M,
 ) (types.M, error) {
 
-	enforceRoleSecurity("find", className, auth)
+	err := enforceRoleSecurity("find", className, auth)
+	if err != nil {
+		return nil, err
+	}
 	query := NewQuery(auth, className, where, options)
 
 	return query.Execute()
 }
 
-// Delete ...
+// Delete 删除指定对象
 func Delete(
 	auth *Auth,
 	className string,
@@ -27,26 +38,28 @@ func Delete(
 ) error {
 
 	if className == "_User" && auth.CouldUpdateUserID(objectID) == false {
-		// TODO 权限不足
+		return errs.E(errs.SessionMissing, "insufficient auth to delete user")
 	}
 
-	enforceRoleSecurity("delete", className, auth)
+	err := enforceRoleSecurity("delete", className, auth)
+	if err != nil {
+		return err
+	}
 
 	var inflatedObject types.M
-
+	// 如果存在删前回调、或者删后回调、或者要删除的属于 _Session 类，则需要获取到要删除的对象数据
 	if TriggerExists(TypeBeforeDelete, className) ||
 		TriggerExists(TypeAfterDelete, className) ||
 		className == "_Session" {
-		// TODO 处理错误
-		response, _ := Find(auth, className, types.M{"objectId": objectID}, types.M{})
-		if utils.HasResults(response) == false {
-			// TODO 未找到要删除的对象
+		response, err := Find(auth, className, types.M{"objectId": objectID}, types.M{})
+		if err != nil || utils.HasResults(response) == false {
+			return errs.E(errs.ObjectNotFound, "Object not found for delete.")
 		}
 
 		result := utils.SliceInterface(response["results"])
 		inflatedObject = utils.MapInterface(result[0])
 		if inflatedObject == nil {
-			// TODO 未找到要删除的对象
+			return errs.E(errs.ObjectNotFound, "Object not found for delete.")
 		}
 	}
 
@@ -55,20 +68,30 @@ func Delete(
 	return destroy.Execute()
 }
 
-// Create ...
+// Create 创建对象
+// 返回数据格式如下：
+// {
+// 	"status":201,
+// 	"response":{...},
+// 	"location":"http://..."
+// }
 func Create(
 	auth *Auth,
 	className string,
 	object types.M,
 ) (types.M, error) {
 
-	enforceRoleSecurity("create", className, auth)
+	err := enforceRoleSecurity("create", className, auth)
+	if err != nil {
+		return nil, err
+	}
 	write := NewWrite(auth, className, nil, object, nil)
 
 	return write.Execute()
 }
 
-// Update ...
+// Update 更新对象
+// 返回更新后的字段，一般只有 updatedAt
 func Update(
 	auth *Auth,
 	className string,
@@ -76,24 +99,27 @@ func Update(
 	object types.M,
 ) (types.M, error) {
 
-	enforceRoleSecurity("update", className, auth)
+	err := enforceRoleSecurity("update", className, auth)
+	if err != nil {
+		return nil, err
+	}
 
 	var originalRestObject types.M
 
+	// 如果存在删前回调、或者删后回调，则需要获取到要删除的对象数据
 	var response types.M
 	if TriggerExists(TypeBeforeSave, className) ||
 		TriggerExists(TypeAfterSave, className) {
-		// TODO 处理错误
-		response, _ = Find(auth, className, types.M{"objectId": objectID}, types.M{})
 
-		if utils.HasResults(response) == false {
-			// TODO 未找到要更新的对象
+		response, err = Find(auth, className, types.M{"objectId": objectID}, types.M{})
+		if err != nil || utils.HasResults(response) == false {
+			return nil, errs.E(errs.ObjectNotFound, "Object not found for update.")
 		}
 
 		result := utils.SliceInterface(response["results"])
 		originalRestObject = utils.MapInterface(result[0])
 		if originalRestObject == nil {
-			// TODO 未找到要更新的对象
+			return nil, errs.E(errs.ObjectNotFound, "Object not found for update.")
 		}
 	}
 
@@ -102,11 +128,15 @@ func Update(
 	return write.Execute()
 }
 
-func enforceRoleSecurity(method string, className string, auth *Auth) {
+// enforceRoleSecurity 对指定的类与操作进行安全校验
+func enforceRoleSecurity(method string, className string, auth *Auth) error {
+	// 非 Master 不得对 _Role 进行操作
 	if className == "_Role" && auth.IsMaster == false {
-		// TODO 权限不足
+		return errs.E(errs.OperationForbidden, "Clients aren't allowed to perform the "+method+" operation on the role collection.")
 	}
+	// 非 Master 不得对 _Installation 进行删除操作
 	if method == "delete" && className == "_Installation" && auth.IsMaster == false {
-		// TODO 权限不足
+		return errs.E(errs.OperationForbidden, "Clients aren't allowed to perform the delete operation on the installation collection.")
 	}
+	return nil
 }

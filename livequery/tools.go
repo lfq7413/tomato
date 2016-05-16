723a864b08856533f04ed0d74b63429d746cb2ac
lfq7413 <lfq7413@gmail.com>
2016-05-16 17:28:36 +0800

    修改 matchesKeyConstraints

--- livequery/tools.go
+++ livequery/tools.go
@@ -5,13 +5,15 @@ func queryHash(query M) string {
 }
 
 func matchesQuery(object, query M) bool {
-	if object["className"].(string) != query["className"].(string) {
-		return false
+
+	if className, ok := query["className"]; ok {
+		if object["className"].(string) != className {
+			return false
+		}
+		return matchesQuery(object, query["where"].(map[string]interface{}))
 	}
 
-	where := query["where"].(map[string]interface{})
-
-	for field, constraints := range where {
+	for field, constraints := range query {
 		if matchesKeyConstraints(object, field, constraints) == false {
 			return false
 		}
@@ -21,5 +23,23 @@ func matchesQuery(object, query M) bool {
 }
 
 func matchesKeyConstraints(object M, key string, constraints interface{}) bool {
-	return false
+	if key == "$or" {
+		if querys, ok := constraints.([]interface{}); ok {
+			for _, query := range querys {
+				if q, ok := query.(map[string]interface{}); ok {
+					if matchesQuery(object, q) {
+						return true
+					}
+				}
+			}
+			return false
+		}
+		return false
+	}
+
+	if key == "$relatedTo" {
+		return false
+	}
+
+	return true
 }

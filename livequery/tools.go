62198f2a05965d793f1693398a6fc76b5b529a81
lfq7413 <lfq7413@gmail.com>
2016-05-17 09:09:39 +0800

    完成 equalObject

--- livequery/tools.go
+++ livequery/tools.go
@@ -70,24 +70,57 @@ func matchesKeyConstraints(object M, key string, constraints interface{}) bool {
 	return true
 }
 
-// equalObject 仅比较基础类型：string float64 bool
+// equalObject 仅比较基础类型：string float64 bool slice map
 func equalObject(i1, i2 interface{}) bool {
 	if v1, ok := i1.(string); ok {
 		if v2, ok := i2.(string); ok {
 			return v1 == v2
 		}
+		return false
 	}
 
 	if v1, ok := i1.(float64); ok {
 		if v2, ok := i2.(float64); ok {
 			return v1 == v2
 		}
+		return false
 	}
 
 	if v1, ok := i1.(bool); ok {
 		if v2, ok := i2.(bool); ok {
 			return v1 == v2
 		}
+		return false
+	}
+
+	if v1, ok := i1.([]interface{}); ok {
+		if v2, ok := i2.([]interface{}); ok {
+			if len(v1) != len(v2) {
+				return false
+			}
+			for i := 0; i < len(v1); i++ {
+				if equalObject(v1[i], v2[i]) == false {
+					return false
+				}
+			}
+			return true
+		}
+		return false
+	}
+
+	if v1, ok := i1.(map[string]interface{}); ok {
+		if v2, ok := i2.(map[string]interface{}); ok {
+			if len(v1) != len(v2) {
+				return false
+			}
+			for k := range v1 {
+				if equalObject(v1[k], v2[k]) == false {
+					return false
+				}
+			}
+			return true
+		}
+		return false
 	}
 
 	return false

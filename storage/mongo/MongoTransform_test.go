dc2743c906cdeef17bd61d72d5c2a4c5ae116b14
lfq7413 <lfq7413@gmail.com>
2016-06-07 16:00:03 +0800

    添加 MongoTransform.go 的单元测试

---
+++ storage/mongo/MongoTransform_test.go
@@ -0,0 +1,91 @@
+package mongo
+
+import "testing"
+
+func Test_transformKey(t *testing.T) {
+	// TODO
+}
+
+func Test_transformKeyValueForUpdate(t *testing.T) {
+	// TODO
+}
+
+func Test_transformQueryKeyValue(t *testing.T) {
+	// TODO
+}
+
+func Test_transformConstraint(t *testing.T) {
+	// TODO
+}
+
+func Test_transformTopLevelAtom(t *testing.T) {
+	// TODO
+}
+
+func Test_transformUpdateOperator(t *testing.T) {
+	// TODO
+}
+
+func Test_parseObjectToMongoObjectForCreate(t *testing.T) {
+	// TODO
+}
+
+func Test_parseObjectKeyValueToMongoObjectKeyValue(t *testing.T) {
+	// TODO
+}
+
+func Test_transformAuthData(t *testing.T) {
+	// TODO
+}
+
+func Test_transformACL(t *testing.T) {
+	// TODO
+}
+
+func Test_transformWhere(t *testing.T) {
+	// TODO
+}
+
+func Test_transformUpdate(t *testing.T) {
+	// TODO
+}
+
+func Test_nestedMongoObjectToNestedParseObject(t *testing.T) {
+	// TODO
+}
+
+func Test_mongoObjectToParseObject(t *testing.T) {
+	// TODO
+}
+
+func Test_untransformACL(t *testing.T) {
+	// TODO
+}
+
+func Test_transformInteriorAtom(t *testing.T) {
+	// TODO
+}
+
+func Test_transformInteriorValue(t *testing.T) {
+	// TODO
+}
+
+func Test_dateCoder(t *testing.T) {
+	// TODO
+}
+
+func Test_bytesCoder(t *testing.T) {
+	// TODO
+}
+
+func Test_geoPointCoder(t *testing.T) {
+	// TODO
+}
+
+func Test_fileCoder(t *testing.T) {
+	// TODO
+}
+
+func Test_valueAsDate(t *testing.T) {
+	// TODO
+}

1fec2442086619ac42ab7936960f437139552f04
lfq7413 <lfq7413@gmail.com>
2016-06-13 11:13:11 +0800

    添加 upsertOne 的单元测试

--- storage/mongo/MongoCollection_test.go
+++ storage/mongo/MongoCollection_test.go
@@ -61,7 +61,49 @@ func Test_insertOne(t *testing.T) {
 }
 
 func Test_upsertOne(t *testing.T) {
-	// TODO
+	db := openDB()
+	defer db.Session.Close()
+	mc := &MongoCollection{collection: db.C("obj")}
+	var docs interface{}
+	var selector interface{}
+	var update interface{}
+	var result []types.M
+	var err error
+	var expect interface{}
+	/********************************************************/
+	docs = types.M{"_id": "001", "name": "joe", "age": 25}
+	mc.insertOne(docs)
+	docs = types.M{"_id": "002", "name": "jack", "age": 30}
+	mc.insertOne(docs)
+	selector = types.M{"name": "joe"}
+	update = types.M{"$set": types.M{"age": 35}}
+	err = mc.upsertOne(selector, update)
+	if err != nil {
+		t.Error("expect:", nil, "get result:", err)
+	}
+	result, err = mc.rawFind(selector, nil)
+	expect = types.M{"_id": "001", "name": "joe", "age": 35}
+	if err != nil || result == nil || len(result) != 1 || reflect.DeepEqual(result[0], expect) == false {
+		t.Error("expect:", expect, "get result:", result, err)
+	}
+	mc.drop()
+	/********************************************************/
+	docs = types.M{"_id": "001", "name": "joe", "age": 25}
+	mc.insertOne(docs)
+	docs = types.M{"_id": "002", "name": "jack", "age": 30}
+	mc.insertOne(docs)
+	selector = types.M{"name": "tom"}
+	update = types.M{"$set": types.M{"_id": "003", "age": 35}}
+	err = mc.upsertOne(selector, update)
+	if err != nil {
+		t.Error("expect:", nil, "get result:", err)
+	}
+	result, err = mc.rawFind(selector, nil)
+	expect = types.M{"_id": "003", "name": "tom", "age": 35}
+	if err != nil || result == nil || len(result) != 1 || reflect.DeepEqual(result[0], expect) == false {
+		t.Error("expect:", expect, "get result:", result, err)
+	}
+	mc.drop()
 }
 
 func Test_updateOne(t *testing.T) {

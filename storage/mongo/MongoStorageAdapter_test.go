2d3a56f1c5f5a2fab2ed25f33f869874f57085f5
lfq7413 <lfq7413@gmail.com>
2016-06-21 16:45:43 +0800

    添加 MongoStorageAdapter 的测试用例

---
+++ storage/mongo/MongoStorageAdapter_test.go
@@ -0,0 +1,91 @@
+package mongo
+
+import "testing"
+
+func Test_ClassExists(t *testing.T) {
+	// TODO
+}
+
+func Test_SetClassLevelPermissions(t *testing.T) {
+	// TODO
+}
+
+func Test_CreateClass(t *testing.T) {
+	// TODO
+}
+
+func Test_AddFieldIfNotExists(t *testing.T) {
+	// TODO
+}
+
+func Test_DeleteClass(t *testing.T) {
+	// TODO
+}
+
+func Test_DeleteAllClasses(t *testing.T) {
+	// TODO
+}
+
+func Test_DeleteFields(t *testing.T) {
+	// TODO
+}
+
+func Test_CreateObject(t *testing.T) {
+	// TODO
+}
+
+func Test_GetClass(t *testing.T) {
+	// TODO
+}
+
+func Test_GetAllClasses(t *testing.T) {
+	// TODO
+}
+
+func Test_getCollectionNames(t *testing.T) {
+	// TODO
+}
+
+func Test_DeleteObjectsByQuery(t *testing.T) {
+	// TODO
+}
+
+func Test_UpdateObjectsByQuery(t *testing.T) {
+	// TODO
+}
+
+func Test_FindOneAndUpdate(t *testing.T) {
+	// TODO
+}
+
+func Test_UpsertOneObject(t *testing.T) {
+	// TODO
+}
+
+func Test_Find(t *testing.T) {
+	// TODO
+}
+
+func Test_AdapterRawFind(t *testing.T) {
+	// TODO
+}
+
+func Test_Count(t *testing.T) {
+	// TODO
+}
+
+func Test_EnsureUniqueness(t *testing.T) {
+	// TODO
+}
+
+func Test_storageAdapterAllCollections(t *testing.T) {
+	// TODO
+}
+
+func Test_convertParseSchemaToMongoSchema(t *testing.T) {
+	// TODO
+}
+
+func Test_mongoSchemaFromFieldsAndClassNameAndCLP(t *testing.T) {
+	// TODO
+}

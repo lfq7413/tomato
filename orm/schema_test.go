85e8ce24e6d41f03eda9572f53e724f7474d92da
lfq7413 <lfq7413@gmail.com>
2016-06-24 13:34:19 +0800

    添加 schema 的测试用例

---
+++ orm/schema_test.go
@@ -0,0 +1,185 @@
+package orm
+
+import "testing"
+
+func Test_AddClassIfNotExists(t *testing.T) {
+	// validateNewClass
+	// convertSchemaToAdapterSchema
+	// convertAdapterSchemaToParseSchema
+	// TODO
+}
+
+func Test_UpdateClass(t *testing.T) {
+	// GetOneSchema
+	// buildMergedSchemaObject
+	// validateSchemaData
+	// deleteField
+	// reloadData
+	// enforceFieldExists
+	// setPermissions
+	// TODO
+}
+
+func Test_deleteField(t *testing.T) {
+	// ClassNameIsValid
+	// fieldNameIsValid
+	// fieldNameIsValidForClass
+	// GetOneSchema
+	// TODO
+}
+
+func Test_validateObject(t *testing.T) {
+	// EnforceClassExists
+	// enforceFieldExists
+	// thenValidateRequiredColumns
+	// TODO
+}
+
+func Test_testBaseCLP(t *testing.T) {
+	// TODO
+}
+
+func Test_validatePermission(t *testing.T) {
+	// testBaseCLP
+	// TODO
+}
+
+func Test_EnforceClassExists(t *testing.T) {
+	// AddClassIfNotExists
+	// TODO
+}
+
+func Test_validateNewClass(t *testing.T) {
+	// InvalidClassNameMessage
+	// validateSchemaData
+	// TODO
+}
+
+func Test_validateSchemaData(t *testing.T) {
+	// fieldNameIsValid
+	// fieldNameIsValidForClass
+	// fieldTypeIsInvalid
+	// validateCLP
+	// TODO
+}
+
+func Test_validateRequiredColumns(t *testing.T) {
+	// TODO
+}
+
+func Test_enforceFieldExists(t *testing.T) {
+	// fieldNameIsValid
+	// getExpectedType
+	// dbTypeMatchesObjectType
+	// TODO
+}
+
+func Test_setPermissions(t *testing.T) {
+	// validateCLP
+	// reloadData
+	// TODO
+}
+
+func Test_HasClass(t *testing.T) {
+	// reloadData
+	// TODO
+}
+
+func Test_getExpectedType(t *testing.T) {
+	// TODO
+}
+
+func Test_reloadData(t *testing.T) {
+	// GetAllClasses
+	// injectDefaultSchema
+	// TODO
+}
+
+func Test_GetAllClasses(t *testing.T) {
+	// injectDefaultSchema
+	// TODO
+}
+
+func Test_GetOneSchema(t *testing.T) {
+	// injectDefaultSchema
+	// TODO
+}
+
+////////////////////////////////////////////////////////////
+
+func Test_thenValidateRequiredColumns(t *testing.T) {
+	// validateRequiredColumns
+	// TODO
+}
+
+func Test_getType(t *testing.T) {
+	// getObjectType
+	// TODO
+}
+
+func Test_getObjectType(t *testing.T) {
+	// TODO
+}
+
+func Test_ClassNameIsValid(t *testing.T) {
+	// joinClassIsValid
+	// fieldNameIsValid
+	// TODO
+}
+
+func Test_InvalidClassNameMessage(t *testing.T) {
+	// TODO
+}
+
+func Test_joinClassIsValid(t *testing.T) {
+	// TODO
+}
+
+func Test_fieldNameIsValid(t *testing.T) {
+	// TODO
+}
+
+func Test_fieldNameIsValidForClass(t *testing.T) {
+	// TODO
+}
+
+func Test_fieldTypeIsInvalid(t *testing.T) {
+	// ClassNameIsValid
+	// InvalidClassNameMessage
+	// TODO
+}
+
+func Test_validateCLP(t *testing.T) {
+	// verifyPermissionKey
+	// TODO
+}
+
+func Test_verifyPermissionKey(t *testing.T) {
+	// TODO
+}
+
+func Test_buildMergedSchemaObject(t *testing.T) {
+	// TODO
+}
+
+func Test_injectDefaultSchema(t *testing.T) {
+	// TODO
+}
+
+func Test_convertSchemaToAdapterSchema(t *testing.T) {
+	// injectDefaultSchema
+	// TODO
+}
+
+func Test_convertAdapterSchemaToParseSchema(t *testing.T) {
+	// TODO
+}
+
+func Test_dbTypeMatchesObjectType(t *testing.T) {
+	// TODO
+}
+
+func Test_Load(t *testing.T) {
+	// reloadData
+	// TODO
+}

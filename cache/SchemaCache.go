40231ab83d16a1196ee355b48d8b249577c61f14
lfq7413 <lfq7413@gmail.com>
2016-07-24 20:43:08 +0800

    添加 SchemaCache 模块

---
+++ cache/SchemaCache.go
@@ -0,0 +1,95 @@
+package cache
+
+import (
+	"github.com/lfq7413/tomato/types"
+	"github.com/lfq7413/tomato/utils"
+)
+
+const mainSchema = "__MAIN_SCHEMA"
+const schemaCachePrefix = "__SCHEMA"
+const allKeys = "__ALL_KEYS"
+
+// SchemaCache ...
+type SchemaCache struct {
+	ttl    int
+	prefix string
+}
+
+// NewSchemaCache ...
+func NewSchemaCache(ttl int) *SchemaCache {
+	return &SchemaCache{
+		ttl:    ttl,
+		prefix: schemaCachePrefix + utils.CreateToken(),
+	}
+}
+
+// Put ...
+func (s *SchemaCache) Put(key string, value interface{}) {
+	var keys map[string]bool
+	v := get(s.prefix + allKeys)
+	if v == nil {
+		keys = map[string]bool{}
+	} else {
+		if m, ok := v.(map[string]bool); ok {
+			keys = m
+		} else {
+			keys = map[string]bool{}
+		}
+	}
+	keys[key] = true
+	put(s.prefix+allKeys, keys, int64(s.ttl))
+	put(key, value, int64(s.ttl))
+}
+
+// GetAllClasses ...
+func (s *SchemaCache) GetAllClasses() types.M {
+	if s.ttl < 0 {
+		return nil
+	}
+	v := get(s.prefix + mainSchema)
+	return utils.M(v)
+}
+
+// SetAllClasses ...
+func (s *SchemaCache) SetAllClasses(schema types.M) {
+	if s.ttl < 0 {
+		return
+	}
+	s.Put(s.prefix+mainSchema, schema)
+}
+
+// SetOneSchema ...
+func (s *SchemaCache) SetOneSchema(className string, schema types.M) {
+	if s.ttl < 0 {
+		return
+	}
+	s.Put(s.prefix+className, schema)
+}
+
+// GetOneSchema ...
+func (s *SchemaCache) GetOneSchema(className string) types.M {
+	if s.ttl < 0 {
+		return nil
+	}
+	v := get(s.prefix + className)
+	return utils.M(v)
+}
+
+// Clear ...
+func (s *SchemaCache) Clear() {
+	var keys map[string]bool
+	v := get(s.prefix + allKeys)
+	if v == nil {
+		return
+	}
+	if m, ok := v.(map[string]bool); ok {
+		keys = m
+	} else {
+		return
+	}
+
+	for key := range keys {
+		del(key)
+	}
+	del(s.prefix + allKeys)
+}

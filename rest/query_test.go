f98512c6287171afc78ec4b1f5a001c0ffedf029
lfq7413 <lfq7413@gmail.com>
2016-07-26 22:14:00 +0800

    添加 query.go 的单元测试

---
+++ rest/query_test.go
@@ -0,0 +1,121 @@
+package rest
+
+import "testing"
+
+func Test_Execute(t *testing.T) {
+	// BuildRestWhere
+	// runFind
+	// runCount
+	// handleInclude
+	// TODO
+}
+
+func Test_BuildRestWhere(t *testing.T) {
+	// getUserAndRoleACL
+	// redirectClassNameForKey
+	// validateClientClassCreation
+	// replaceSelect
+	// replaceDontSelect
+	// replaceInQuery
+	// replaceNotInQuery
+	// TODO
+}
+
+func Test_getUserAndRoleACL(t *testing.T) {
+	// TODO
+}
+
+func Test_redirectClassNameForKey(t *testing.T) {
+	// TODO
+}
+
+func Test_validateClientClassCreation(t *testing.T) {
+	// TODO
+}
+
+func Test_replaceSelect(t *testing.T) {
+	// findObjectWithKey
+	// NewQuery
+	// Execute
+	// transformSelect
+	// TODO
+}
+
+func Test_replaceDontSelect(t *testing.T) {
+	// findObjectWithKey
+	// NewQuery
+	// Execute
+	// transformDontSelect
+	// TODO
+}
+
+func Test_replaceInQuery(t *testing.T) {
+	// findObjectWithKey
+	// NewQuery
+	// Execute
+	// transformInQuery
+	// TODO
+}
+
+func Test_replaceNotInQuery(t *testing.T) {
+	// findObjectWithKey
+	// NewQuery
+	// Execute
+	// transformNotInQuery
+	// TODO
+}
+
+func Test_runFind(t *testing.T) {
+	// TODO
+}
+
+func Test_runCount(t *testing.T) {
+	// TODO
+}
+
+func Test_handleInclude(t *testing.T) {
+	// includePath
+	// TODO
+}
+
+/////////////////////////////////////////////////////////////////
+
+func Test_NewQuery(t *testing.T) {
+	// TODO
+}
+
+func Test_includePath(t *testing.T) {
+	// findPointers
+	// NewQuery
+	// Execute
+	// replacePointers
+	// TODO
+}
+
+func Test_findPointers(t *testing.T) {
+	// TODO
+}
+
+func Test_replacePointers(t *testing.T) {
+	// TODO
+}
+
+func Test_findObjectWithKey(t *testing.T) {
+	// TODO
+}
+
+func Test_transformSelect(t *testing.T) {
+	// TODO
+}
+
+func Test_transformDontSelect(t *testing.T) {
+	// TODO
+}
+
+func Test_transformInQuery(t *testing.T) {
+	// TODO
+}
+
+func Test_transformNotInQuery(t *testing.T) {
+	// TODO
+}

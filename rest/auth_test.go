0209a30d6ed6e6e4d2af6ab62e0abd83216e0278
lfq7413 <lfq7413@gmail.com>
2016-08-06 21:37:34 +0800

    添加 auth.go 的单元测试

---
+++ rest/auth_test.go
@@ -0,0 +1,25 @@
+package rest
+
+import "testing"
+
+func Test_GetAuthForSessionToken(t *testing.T) {
+	// TODO
+}
+
+func Test_CouldUpdateUserID(t *testing.T) {
+	// TODO
+}
+
+func Test_GetUserRoles(t *testing.T) {
+	// loadRoles
+	// TODO
+}
+
+func Test_loadRoles(t *testing.T) {
+	// getAllRoleNamesForID
+	// TODO
+}
+
+func Test_getAllRoleNamesForID(t *testing.T) {
+	// TODO
+}

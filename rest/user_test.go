af9411837c2522c8cc924c4d430b4bb8e8733398
lfq7413 <lfq7413@gmail.com>
2016-08-09 22:05:28 +0800

    添加 user.go 的单元测试

---
+++ rest/user_test.go
@@ -0,0 +1,51 @@
+package rest
+
+import "testing"
+
+func Test_SetEmailVerifyToken(t *testing.T) {
+	// TODO
+}
+
+func Test_SendVerificationEmail(t *testing.T) {
+	// getUserIfNeeded
+	// TODO
+}
+
+func Test_getUserIfNeeded(t *testing.T) {
+	// TODO
+}
+
+func Test_defaultVerificationEmail(t *testing.T) {
+	// TODO
+}
+
+func Test_SendPasswordResetEmail(t *testing.T) {
+	// setPasswordResetToken
+	// TODO
+}
+
+func Test_setPasswordResetToken(t *testing.T) {
+	// TODO
+}
+
+func Test_defaultResetPasswordEmail(t *testing.T) {
+	// TODO
+}
+
+func Test_VerifyEmail(t *testing.T) {
+	// TODO
+}
+
+func Test_CheckResetTokenValidity(t *testing.T) {
+	// TODO
+}
+
+func Test_UpdatePassword(t *testing.T) {
+	// CheckResetTokenValidity
+	// updateUserPassword
+	// TODO
+}
+
+func Test_updateUserPassword(t *testing.T) {
+	// TODO
+}

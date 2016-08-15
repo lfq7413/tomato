11cb32f7e98f90304012645c5e2eda57ba7413eb
lfq7413 <lfq7413@gmail.com>
2016-08-15 17:05:57 +0800

    添加 write.go 的测试用例

---
+++ rest/write_test.go
@@ -0,0 +1,119 @@
+package rest
+
+import "testing"
+
+func Test_NewWrite(t *testing.T) {
+	// TODO
+}
+
+func Test_Execute_Write(t *testing.T) {
+	// TODO
+}
+
+func Test_getUserAndRoleACL_Write(t *testing.T) {
+	// TODO
+}
+
+func Test_validateClientClassCreation_Write(t *testing.T) {
+	// TODO
+}
+
+func Test_validateSchema(t *testing.T) {
+	// TODO
+}
+
+func Test_handleInstallation(t *testing.T) {
+	// TODO
+}
+
+func Test_handleSession(t *testing.T) {
+	// TODO
+}
+
+func Test_validateAuthData(t *testing.T) {
+	// handleAuthData
+	// TODO
+}
+
+func Test_runBeforeTrigger(t *testing.T) {
+	// sanitizedData
+	// TODO
+}
+
+func Test_setRequiredFieldsIfNeeded(t *testing.T) {
+	// TODO
+}
+
+func Test_transformUser(t *testing.T) {
+	// objectID
+	// TODO
+}
+
+func Test_expandFilesForExistingObjects(t *testing.T) {
+	// TODO
+}
+
+func Test_runDatabaseOperation(t *testing.T) {
+	// updateResponseWithData
+	// location
+	// objectID
+	// TODO
+}
+
+func Test_createSessionTokenIfNeeded(t *testing.T) {
+	// createSessionToken
+	// TODO
+}
+
+func Test_handleFollowup(t *testing.T) {
+	// createSessionToken
+	// objectID
+	// TODO
+}
+
+func Test_runAfterTrigger(t *testing.T) {
+	// sanitizedData
+	// TODO
+}
+
+func Test_cleanUserAuthData(t *testing.T) {
+	// TODO
+}
+
+/////////////////////////////////////////////////////////////
+
+func Test_handleAuthData(t *testing.T) {
+	// handleAuthDataValidation
+	// findUsersWithAuthData
+	// location
+	// TODO
+}
+
+func Test_handleAuthDataValidation(t *testing.T) {
+	// TODO
+}
+
+func Test_findUsersWithAuthData(t *testing.T) {
+	// TODO
+}
+
+func Test_createSessionToken(t *testing.T) {
+	// objectID
+	// TODO
+}
+
+func Test_location(t *testing.T) {
+	// TODO
+}
+
+func Test_objectID(t *testing.T) {
+	// TODO
+}
+
+func Test_sanitizedData(t *testing.T) {
+	// TODO
+}
+
+func Test_updateResponseWithData(t *testing.T) {
+	// TODO
+}

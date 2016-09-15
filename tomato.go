adc83e07cc08a967e7c13b807e980d944eb0ab4c
lfq7413 <lfq7413@gmail.com>
2016-09-15 21:55:10 +0800

    添加 CrossDomain 支持 (#2)
    thanks to umdp

--- tomato.go
+++ tomato.go
@@ -5,6 +5,8 @@ import (
 	_ "github.com/lfq7413/tomato/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/lfq7413/tomato/livequery"
 	"github.com/lfq7413/tomato/orm"
 	"github.com/lfq7413/tomato/storage"
@@ -22,7 +24,11 @@ func Run() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
 	beego.ErrorController(&controllers.ErrorController{})
+
+	allowCrossDomain()
+
 	beego.Run()
 }
 
@@ -32,4 +38,22 @@ func RunLiveQueryServer() {
 	args["pattern"] = "/livequery"
 	args["addr"] = "/8089"
 	livequery.Run(args)
+}
+
+func allowCrossDomain() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Authorization", "Access-Control-Allow-Origin"},
+		ExposeHeaders: []string{"Access-Control-Allow-Headers", "X-Parse-Master-Key", "X-Parse-REST-API-Key",
+			"X-Parse-Javascript-Key", "X-Parse-Application-Id", "X-Parse-Client-Version", "X-Parse-Session-Token",
+			"X-Requested-With", "X-Parse-Revocable-Session", "Content-Type"},
+		AllowCredentials: true,
+	}))
+	beego.InsertFilter("*", beego.BeforeRouter, func(ctx *context.Context) {
+		if ctx.Input.Method() == "OPTIONS" {
+			ctx.Output.SetStatus(200)
+			ctx.ResponseWriter.Started = true
+		}
+	})
 }

3a2bc6e376e618b0a9e6d6df8a2a724357de4e32
lfq7413 <lfq7413@gmail.com>
2016-09-26 14:29:13 +0800

    添加 notLocked 的单元测试

--- rest/AccountLockout_test.go
+++ rest/AccountLockout_test.go
@@ -2,10 +2,12 @@ package rest
 
 import (
 	"reflect"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/lfq7413/tomato/config"
+	"github.com/lfq7413/tomato/errs"
 	"github.com/lfq7413/tomato/orm"
 	"github.com/lfq7413/tomato/types"
 	"github.com/lfq7413/tomato/utils"
@@ -19,7 +21,102 @@ func Test_HandleLoginAttempt(t *testing.T) {
 }
 
 func Test_notLocked(t *testing.T) {
-	// TODO
+	var username string
+	var object, schema types.M
+	var accountLockout *AccountLockout
+	var err, expectErr error
+	var expiresAtStr string
+	/*****************************************************************/
+	config.TConfig.AccountLockoutThreshold = 3
+	config.TConfig.AccountLockoutDuration = 5
+	expiresAtStr = utils.TimetoString(time.Now().UTC().Add(time.Duration(config.TConfig.AccountLockoutDuration) * time.Minute))
+	initEnv()
+	username = "joe"
+	schema = types.M{
+		"fields": types.M{
+			"username": types.M{"type": "String"},
+			"password": types.M{"type": "String"},
+		},
+	}
+	orm.Adapter.CreateClass("_User", schema)
+	object = types.M{
+		"objectId": "01",
+		"username": username,
+		"_account_lockout_expires_at": types.M{
+			"__type": "Date",
+			"iso":    expiresAtStr,
+		},
+		"_failed_login_count": 3,
+	}
+	orm.Adapter.CreateObject("_User", schema, object)
+	accountLockout = NewAccountLockout(username)
+	err = accountLockout.notLocked()
+	expectErr = errs.E(errs.ObjectNotFound, "Your account is locked due to multiple failed login attempts. Please try again after "+
+		strconv.Itoa(config.TConfig.AccountLockoutDuration)+" minute(s)")
+	if reflect.DeepEqual(expectErr, err) == false {
+		t.Error("expect:", expectErr, "result:", err)
+	}
+	orm.TomatoDBController.DeleteEverything()
+	/*****************************************************************/
+	config.TConfig.AccountLockoutThreshold = 3
+	config.TConfig.AccountLockoutDuration = 5
+	expiresAtStr = utils.TimetoString(time.Now().UTC().Add(time.Duration(config.TConfig.AccountLockoutDuration) * time.Minute))
+	initEnv()
+	username = "joe"
+	schema = types.M{
+		"fields": types.M{
+			"username": types.M{"type": "String"},
+			"password": types.M{"type": "String"},
+		},
+	}
+	orm.Adapter.CreateClass("_User", schema)
+	object = types.M{
+		"objectId": "01",
+		"username": username,
+		"_account_lockout_expires_at": types.M{
+			"__type": "Date",
+			"iso":    expiresAtStr,
+		},
+		"_failed_login_count": 1,
+	}
+	orm.Adapter.CreateObject("_User", schema, object)
+	accountLockout = NewAccountLockout(username)
+	err = accountLockout.notLocked()
+	expectErr = nil
+	if reflect.DeepEqual(expectErr, err) == false {
+		t.Error("expect:", expectErr, "result:", err)
+	}
+	orm.TomatoDBController.DeleteEverything()
+	/*****************************************************************/
+	config.TConfig.AccountLockoutThreshold = 3
+	config.TConfig.AccountLockoutDuration = 5
+	expiresAtStr = utils.TimetoString(time.Now().UTC().Add(-time.Duration(config.TConfig.AccountLockoutDuration) * time.Minute))
+	initEnv()
+	username = "joe"
+	schema = types.M{
+		"fields": types.M{
+			"username": types.M{"type": "String"},
+			"password": types.M{"type": "String"},
+		},
+	}
+	orm.Adapter.CreateClass("_User", schema)
+	object = types.M{
+		"objectId": "01",
+		"username": username,
+		"_account_lockout_expires_at": types.M{
+			"__type": "Date",
+			"iso":    expiresAtStr,
+		},
+		"_failed_login_count": 3,
+	}
+	orm.Adapter.CreateObject("_User", schema, object)
+	accountLockout = NewAccountLockout(username)
+	err = accountLockout.notLocked()
+	expectErr = nil
+	if reflect.DeepEqual(expectErr, err) == false {
+		t.Error("expect:", expectErr, "result:", err)
+	}
+	orm.TomatoDBController.DeleteEverything()
 }
 
 func Test_setFailedLoginCount(t *testing.T) {

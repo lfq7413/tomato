fac48435d3a33c409f63223416631bbe9c36f0ea
lfq7413 <lfq7413@gmail.com>
2016-09-26 11:23:30 +0800

    添加 isFailedLoginCountSet 的单元测试

--- rest/AccountLockout_test.go
+++ rest/AccountLockout_test.go
@@ -1,6 +1,11 @@
 package rest
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/lfq7413/tomato/orm"
+	"github.com/lfq7413/tomato/types"
+)
 
 func Test_HandleLoginAttempt(t *testing.T) {
 	// TODO
@@ -39,5 +44,52 @@ func Test_setLockoutExpiration(t *testing.T) {
 }
 
 func Test_isFailedLoginCountSet(t *testing.T) {
-	// TODO
+	var username string
+	var object, schema types.M
+	var accountLockout *AccountLockout
+	var isSet bool
+	var err error
+	/*****************************************************************/
+	initEnv()
+	username = "joe"
+	schema = types.M{
+		"fields": types.M{
+			"username": types.M{"type": "String"},
+			"password": types.M{"type": "String"},
+		},
+	}
+	orm.Adapter.CreateClass("_User", schema)
+	object = types.M{
+		"objectId": "01",
+		"username": username,
+	}
+	orm.Adapter.CreateObject("_User", schema, object)
+	accountLockout = NewAccountLockout(username)
+	isSet, err = accountLockout.isFailedLoginCountSet()
+	if err != nil || isSet != false {
+		t.Error("expect:", false, "result:", isSet, err)
+	}
+	orm.TomatoDBController.DeleteEverything()
+	/*****************************************************************/
+	initEnv()
+	username = "joe"
+	schema = types.M{
+		"fields": types.M{
+			"username": types.M{"type": "String"},
+			"password": types.M{"type": "String"},
+		},
+	}
+	orm.Adapter.CreateClass("_User", schema)
+	object = types.M{
+		"objectId":            "01",
+		"username":            username,
+		"_failed_login_count": 3,
+	}
+	orm.Adapter.CreateObject("_User", schema, object)
+	accountLockout = NewAccountLockout(username)
+	isSet, err = accountLockout.isFailedLoginCountSet()
+	if err != nil || isSet != true {
+		t.Error("expect:", true, "result:", isSet, err)
+	}
+	orm.TomatoDBController.DeleteEverything()
 }

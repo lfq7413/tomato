cf56a0eb797d70ba4154837ef37daed10cff66b5
lfq7413 <lfq7413@gmail.com>
2016-09-26 14:04:57 +0800

    添加 incrementFailedLoginCount 的单元测试

--- rest/AccountLockout_test.go
+++ rest/AccountLockout_test.go
@@ -40,7 +40,44 @@ func Test_initFailedLoginCount(t *testing.T) {
 }
 
 func Test_incrementFailedLoginCount(t *testing.T) {
-	// TODO
+	var username string
+	var object, schema types.M
+	var accountLockout *AccountLockout
+	var err error
+	var results, expect []types.M
+	/*****************************************************************/
+	initEnv()
+	username = "joe"
+	schema = types.M{
+		"fields": types.M{
+			"username": types.M{"type": "String"},
+			"password": types.M{"type": "String"},
+		},
+	}
+	orm.Adapter.CreateClass("_User", schema)
+	object = types.M{
+		"objectId":            "01",
+		"username":            username,
+		"_failed_login_count": 0,
+	}
+	orm.Adapter.CreateObject("_User", schema, object)
+	accountLockout = NewAccountLockout(username)
+	err = accountLockout.incrementFailedLoginCount()
+	if err != nil {
+		t.Error("expect:", nil, "result:", err)
+	}
+	results, err = orm.Adapter.Find("_User", schema, types.M{}, types.M{})
+	expect = []types.M{
+		types.M{
+			"objectId":            "01",
+			"username":            username,
+			"_failed_login_count": 1,
+		},
+	}
+	if reflect.DeepEqual(expect, results) == false {
+		t.Error("expect:", expect, "result:", results)
+	}
+	orm.TomatoDBController.DeleteEverything()
 }
 
 func Test_setLockoutExpiration(t *testing.T) {

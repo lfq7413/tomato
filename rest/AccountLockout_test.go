d7d1d0418e6db9906e4eba94ffe60901f5bb0279
lfq7413 <lfq7413@gmail.com>
2016-09-26 10:31:04 +0800

    添加 AccountLockout.go 的单元测试

---
+++ rest/AccountLockout_test.go
@@ -0,0 +1,43 @@
+package rest
+
+import "testing"
+
+func Test_HandleLoginAttempt(t *testing.T) {
+	// TODO
+	// notLocked
+	// setFailedLoginCount
+	// handleFailedLoginAttempt
+}
+
+func Test_notLocked(t *testing.T) {
+	// TODO
+}
+
+func Test_setFailedLoginCount(t *testing.T) {
+	// TODO
+}
+
+func Test_handleFailedLoginAttempt(t *testing.T) {
+	// TODO
+	// initFailedLoginCount
+	// incrementFailedLoginCount
+	// setLockoutExpiration
+}
+
+func Test_initFailedLoginCount(t *testing.T) {
+	// TODO
+	// isFailedLoginCountSet
+	// setFailedLoginCount
+}
+
+func Test_incrementFailedLoginCount(t *testing.T) {
+	// TODO
+}
+
+func Test_setLockoutExpiration(t *testing.T) {
+	// TODO
+}
+
+func Test_isFailedLoginCountSet(t *testing.T) {
+	// TODO
+}

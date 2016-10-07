23dfa0518bc2e8f4bd4b192cd312395f62373c82
lfq7413 <lfq7413@gmail.com>
2016-10-07 19:53:25 +0800

    添加 event emitter pubsub 测试

--- livequery/pubsub/event_emitter_pub_sub_test.go
+++ livequery/pubsub/event_emitter_pub_sub_test.go
@@ -6,12 +6,12 @@ import (
 	"time"
 )
 
-func Test_redis(t *testing.T) {
-	sub := createRedisSubscriber("192.168.99.100:6379")
-	pub := createRedisPublisher("192.168.99.100:6379")
+func Test_EventEmitter(t *testing.T) {
+	sub := createEventEmitterSubscriber()
+	pub := createEventEmitterPublisher()
 	sub.Subscribe("message")
 	sub.On("message", func(args ...string) {
-		fmt.Println("redis msg", args)
+		fmt.Println("event emitter msg", args)
 	})
 	pub.Publish("message", "hello")
 	time.Sleep(500 * time.Millisecond)

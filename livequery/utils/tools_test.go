b0bc587e92c726ade92e38a803b0d420cca31f3f
lfq7413 <lfq7413@gmail.com>
2016-10-12 14:25:18 +0800

    添加 QueryHash 的测试用例

--- livequery/utils/tools_test.go
+++ livequery/utils/tools_test.go
@@ -5,7 +5,73 @@ import "reflect"
 import tp "github.com/lfq7413/tomato/livequery/t"
 
 func Test_QueryHash(t *testing.T) {
-	// TODO
+	data := []struct {
+		query  tp.M
+		expect string
+	}{
+		{
+			query: tp.M{
+				"className": "Player",
+				"where":     map[string]interface{}{},
+			},
+			expect: "Player:|[]",
+		},
+		{
+			query: tp.M{
+				"className": "Player",
+				"where": map[string]interface{}{
+					"name": "joe",
+				},
+			},
+			expect: "Player:name|[joe]",
+		},
+		{
+			query: tp.M{
+				"className": "Player",
+				"where": map[string]interface{}{
+					"name": "joe",
+					"age":  12,
+				},
+			},
+			expect: "Player:age,name|[12 joe]",
+		},
+		{
+			query: tp.M{
+				"className": "Player",
+				"where": map[string]interface{}{"$or": []interface{}{
+					map[string]interface{}{
+						"name": "joe",
+					},
+					map[string]interface{}{
+						"age": "12",
+					},
+				}},
+			},
+			expect: "Player:age,name|[joe 12]",
+		},
+		{
+			query: tp.M{
+				"className": "Player",
+				"where": map[string]interface{}{"$or": []interface{}{
+					map[string]interface{}{
+						"name": "joe",
+					},
+					map[string]interface{}{
+						"name": "joe",
+						"age":  "12",
+					},
+				}},
+			},
+			expect: "Player:age,name|[joe 12 joe]",
+		},
+	}
+
+	for _, d := range data {
+		result := QueryHash(d.query)
+		if reflect.DeepEqual(d.expect, result) == false {
+			t.Error("expect:", d.expect, "result:", result)
+		}
+	}
 }
 
 func Test_flattenOrQueries(t *testing.T) {

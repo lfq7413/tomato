e83390643ee49b0c4959919ac2a92021694c8664
lfq7413 <lfq7413@gmail.com>
2016-10-12 14:09:53 +0800

    添加 flattenOrQueries 的测试用例

---
+++ livequery/utils/tools_test.go
@@ -0,0 +1,107 @@
+package utils
+
+import "testing"
+import "reflect"
+import tp "github.com/lfq7413/tomato/livequery/t"
+
+func Test_QueryHash(t *testing.T) {
+	// TODO
+}
+
+func Test_flattenOrQueries(t *testing.T) {
+	data := []struct {
+		where  tp.M
+		expect []map[string]interface{}
+	}{
+		{
+			where:  tp.M{},
+			expect: nil,
+		},
+		{
+			where:  tp.M{"$or": 1024},
+			expect: nil,
+		},
+		{
+			where: tp.M{"$or": []interface{}{
+				map[string]interface{}{
+					"name": "joe",
+				},
+				1024,
+			}},
+			expect: []map[string]interface{}{
+				map[string]interface{}{
+					"name": "joe",
+				},
+			},
+		},
+		{
+			where: tp.M{"$or": []interface{}{
+				map[string]interface{}{
+					"name": "joe",
+				},
+				map[string]interface{}{
+					"age": "20",
+				},
+			}},
+			expect: []map[string]interface{}{
+				map[string]interface{}{
+					"name": "joe",
+				},
+				map[string]interface{}{
+					"age": "20",
+				},
+			},
+		},
+	}
+
+	for _, d := range data {
+		result := flattenOrQueries(d.where)
+		if reflect.DeepEqual(d.expect, result) == false {
+			t.Error("expect:", d.expect, "result:", result)
+		}
+	}
+}
+
+func Test_MatchesQuery(t *testing.T) {
+	// TODO
+	// matchesKeyConstraints
+}
+
+func Test_matchesKeyConstraints(t *testing.T) {
+	// TODO
+	// equalObject
+	// compareNumber
+	// inSlice
+	// compareRegexp
+	// compareGeoPoint
+	// compareBox
+}
+
+func Test_compareBox(t *testing.T) {
+	// TODO
+}
+
+func Test_compareGeoPoint(t *testing.T) {
+	// TODO
+}
+
+func Test_distance(t *testing.T) {
+	// TODO
+}
+
+func Test_compareRegexp(t *testing.T) {
+	// TODO
+}
+
+func Test_inSlice(t *testing.T) {
+	// TODO
+	// equalObject
+}
+
+func Test_compareNumber(t *testing.T) {
+	// TODO
+}
+
+func Test_equalObject(t *testing.T) {
+	// TODO
+}

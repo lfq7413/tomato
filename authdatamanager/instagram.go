92b1f9b73f827bf0d31c6a3a441e8f5bb25a6e27
lfq7413 <lfq7413@gmail.com>
2016-10-17 10:12:16 +0800

    完成 instagram token 校验

---
+++ authdatamanager/instagram.go
@@ -0,0 +1,24 @@
+package authdatamanager
+
+import (
+	"github.com/lfq7413/tomato/errs"
+	"github.com/lfq7413/tomato/types"
+	"github.com/lfq7413/tomato/utils"
+)
+
+type instagram struct{}
+
+func (a instagram) ValidateAuthData(authData types.M) error {
+	host := "https://api.instagram.com/v1/"
+	path := "users/self/?access_token=" + utils.S(authData["access_token"])
+	data, err := request(host+path, nil)
+	if err != nil {
+		return errs.E(errs.ObjectNotFound, "Failed to validate this access token with Instagram.")
+	}
+	if d := utils.M(data["data"]); d != nil {
+		if d["id"] != nil && utils.S(d["id"]) == utils.S(authData["id"]) {
+			return nil
+		}
+	}
+	return errs.E(errs.ObjectNotFound, "Instagram auth is invalid for this user.")
+}

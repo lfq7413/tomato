a7fe1c9262ea160488246c0cc7d7f9dfa6810410
lfq7413 <lfq7413@gmail.com>
2016-10-20 17:30:30 +0800

    完善 OAuth1Client.go

--- authdatamanager/OAuth1Client.go
+++ authdatamanager/OAuth1Client.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"sort"
+	"strings"
 
 	"github.com/lfq7413/tomato/types"
 )
@@ -21,6 +23,15 @@ type OAuth struct {
 // Get ...
 func (o *OAuth) Get(path string, params map[string]string) (types.M, error) {
 	req, err := o.buildRequest("GET", path, params, nil)
+	if err != nil {
+		return nil, err
+	}
+	return o.Send(req)
+}
+
+// Post ...
+func (o *OAuth) Post(path string, params map[string]string, body map[string]string) (types.M, error) {
+	req, err := o.buildRequest("POST", path, params, body)
 	if err != nil {
 		return nil, err
 	}
@@ -50,5 +61,52 @@ func (o *OAuth) Send(req *http.Request) (types.M, error) {
 }
 
 func (o *OAuth) buildRequest(method, path string, params, body map[string]string) (*http.Request, error) {
-	return nil, nil
+	// TODO
+	if len(params) > 0 {
+		path = path + "?" + buildParameterString(params)
+	}
+
+	request, err := http.NewRequest(method, o.Host+path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	oauthParams := map[string]string{}
+	if o.OAuthParams != nil {
+		oauthParams = o.OAuthParams
+	}
+	oauthParams["oauth_consumer_key"] = o.ConsumerKey
+	if o.AuthToken != "" {
+		oauthParams["oauth_token"] = o.AuthToken
+	}
+
+	request = signRequest(request, oauthParams, o.ConsumerSecret, o.AuthTokenSecret)
+
+	return request, nil
+}
+
+func buildParameterString(obj map[string]string) string {
+	if len(obj) == 0 {
+		return ""
+	}
+	keys := []string{}
+	for k := range obj {
+		keys = append(keys, k)
+	}
+	sort.Sort(sort.StringSlice(keys))
+	result := []string{}
+	for _, k := range keys {
+		result = append(result, k+"="+encode(obj[k]))
+	}
+	return strings.Join(result, "&")
+}
+
+func encode(str string) string {
+	// TODO
+	return str
+}
+
+func signRequest(req *http.Request, oauthParameters map[string]string, consumerSecret, authTokenSecret string) *http.Request {
+	// TODO
+	return req
 }
---
+++ authdatamanager/qq_test.go
@@ -0,0 +1,18 @@
+package authdatamanager
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/lfq7413/tomato/types"
+)
+
+func Test_qq_ValidateAuthData(t *testing.T) {
+	authData := types.M{
+		"access_token": "abc",
+		"id":           "123",
+	}
+	a := yixin{}
+	err := a.ValidateAuthData(authData, nil)
+	fmt.Println(err)
+}

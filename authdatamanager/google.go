eb91db7c0deed0de38384b1cf45b66640a8f3b70
lfq7413 <lfq7413@gmail.com>
2016-11-05 23:35:15 +0800

    修改 Google 授权验证方式

--- authdatamanager/google.go
+++ authdatamanager/google.go
@@ -9,13 +9,39 @@ import (
 type google struct{}
 
 func (a google) ValidateAuthData(authData types.M, options types.M) error {
+	var err error
+	if utils.S(authData["id_token"]) != "" {
+		err = a.validateIDToken(utils.S(authData["id"]), utils.S(authData["id_token"]))
+	} else {
+		err = a.validateAuthToken(utils.S(authData["id"]), utils.S(authData["access_token"]))
+		if err != nil {
+			err = a.validateIDToken(utils.S(authData["id"]), utils.S(authData["access_token"]))
+		}
+	}
+	return err
+}
+
+func (a google) validateIDToken(id, token string) error {
 	host := "https://www.googleapis.com/oauth2/v3/"
-	path := "tokeninfo?id_token=" + utils.S(authData["access_token"])
+	path := "tokeninfo?id_token=" + token
 	data, err := request(host+path, nil)
 	if err != nil {
 		return errs.E(errs.ObjectNotFound, "Failed to validate this access token with Google.")
 	}
-	if data["sub"] != nil && utils.S(data["sub"]) == utils.S(authData["id"]) {
+	if data["sub"] != nil && utils.S(data["sub"]) == id {
+		return nil
+	}
+	return errs.E(errs.ObjectNotFound, "Google auth is invalid for this user.")
+}
+
+func (a google) validateAuthToken(id, token string) error {
+	host := "https://www.googleapis.com/oauth2/v3/"
+	path := "tokeninfo?access_token=" + token
+	data, err := request(host+path, nil)
+	if err != nil {
+		return errs.E(errs.ObjectNotFound, "Failed to validate this access token with Google.")
+	}
+	if data["user_id"] != nil && utils.S(data["user_id"]) == id {
 		return nil
 	}
 	return errs.E(errs.ObjectNotFound, "Google auth is invalid for this user.")

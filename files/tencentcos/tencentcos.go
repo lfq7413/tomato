c3d297a90109ea99d6e08d60635b429c41e674e6
lfq7413 <lfq7413@gmail.com>
2016-11-20 19:11:21 +0800

    添加存储结构体

--- files/tencentcos/tencentcos.go
+++ files/tencentcos/tencentcos.go
@@ -1,1 +1,11 @@
 package tencentcos
+
+// COS ...
+// 参考链接：https://www.qcloud.com/doc/api/264/3627
+type COS struct {
+	AppID     string
+	SecretID  string
+	SecretKey string
+	Bucket    string
+	URI       string
+}

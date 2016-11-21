211f4f7824fe3100c2dbd8918d160ad80ac0ce77
lfq7413 <lfq7413@gmail.com>
2016-11-21 11:07:24 +0800

    添加签名方法

--- files/tencentcos/tencentcos.go
+++ files/tencentcos/tencentcos.go
@@ -1,5 +1,12 @@
 package tencentcos
 
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+)
+
 // COS ...
 // 参考链接：https://www.qcloud.com/doc/api/264/3627
 type COS struct {
@@ -7,5 +14,35 @@ type COS struct {
 	SecretID  string
 	SecretKey string
 	Bucket    string
-	URI       string
+}
+
+// URI 文件云服务器域名
+var URI = "web.file.myqcloud.com"
+
+type sig struct {
+	appID       string
+	bucket      string
+	secretID    string
+	expiredTime string
+	currentTime string
+	rand        string
+	fileid      string
+}
+
+func (s sig) getMultiEffectSignature(sk string) string {
+	sig := fmt.Sprintf("a=%s&b=%s&k=%s&e=%s&t=%s&r=%s&f=", s.appID, s.bucket, s.secretID, s.expiredTime, s.currentTime, s.rand)
+	return getHash(sig, sk)
+}
+
+func (s sig) getOnceSignature(sk string) string {
+	sig := fmt.Sprintf("a=%s&b=%s&k=%s&e=%s&t=%s&r=%s&f=%s", s.appID, s.bucket, s.secretID, "0", s.currentTime, s.rand, s.fileid)
+	return getHash(sig, sk)
+}
+
+func getHash(sig, sk string) string {
+	mac := hmac.New(sha1.New, []byte(sk))
+	mac.Write([]byte(sig))
+	signTmp := mac.Sum(nil)
+	sign := base64.StdEncoding.EncodeToString(append(signTmp, []byte(sig)...))
+	return sign
 }
---
+++ files/tencentcos/tencentcos_test.go
@@ -0,0 +1,37 @@
+package tencentcos
+
+import "testing"
+
+// a=200001&b=newbucket&k=AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv&e=1470737000&t=1470736940&r=490258943&f=
+// a=200001&b=newbucket&k=AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv&e=0&t=1470736940&r=490258943&f=/200001/newbucket/tencent_test.jpg
+func Test_sig(t *testing.T) {
+	s := sig{
+		appID:       "200001",
+		bucket:      "newbucket",
+		secretID:    "AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv",
+		expiredTime: "1470737000",
+		currentTime: "1470736940",
+		rand:        "490258943",
+		fileid:      "",
+	}
+	ss := s.getMultiEffectSignature("bLcPnl88WU30VY57ipRhSePfPdOfSruK")
+	expect := "v6+um3VE3lxGz97PmnSg6+/V9PZhPTIwMDAwMSZiPW5ld2J1Y2tldCZrPUFLSURVZkxVRVVpZ1FpWHFtN0NWU3NwS0pudWFpSUt0eHFBdiZlPTE0NzA3MzcwMDAmdD0xNDcwNzM2OTQwJnI9NDkwMjU4OTQzJmY9"
+	if ss != expect {
+		t.Error("expect:", expect, "\n", "result:", ss)
+	}
+
+	s = sig{
+		appID:       "200001",
+		bucket:      "newbucket",
+		secretID:    "AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv",
+		expiredTime: "0",
+		currentTime: "1470736940",
+		rand:        "490258943",
+		fileid:      "/200001/newbucket/tencent_test.jpg",
+	}
+	ss = s.getOnceSignature("bLcPnl88WU30VY57ipRhSePfPdOfSruK")
+	expect = "CkZ0/gWkHy3f76ER7k6yXgzq7w1hPTIwMDAwMSZiPW5ld2J1Y2tldCZrPUFLSURVZkxVRVVpZ1FpWHFtN0NWU3NwS0pudWFpSUt0eHFBdiZlPTAmdD0xNDcwNzM2OTQwJnI9NDkwMjU4OTQzJmY9LzIwMDAwMS9uZXdidWNrZXQvdGVuY2VudF90ZXN0LmpwZw=="
+	if ss != expect {
+		t.Error("expect:", expect, "\n", "result:", ss)
+	}
+}

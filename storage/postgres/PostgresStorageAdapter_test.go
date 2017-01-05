d2eced96b10c06280bd1fa8c11d816920e5305ed
lfq7413 <lfq7413@gmail.com>
2017-01-05 10:38:45 +0800

    添加 toPostgresValue transformValue 的单元测试用例

--- storage/postgres/PostgresStorageAdapter_test.go
+++ storage/postgres/PostgresStorageAdapter_test.go
@@ -1,9 +1,8 @@
 package postgres
 
 import (
-	"testing"
-
 	"reflect"
+	"testing"
 
 	"github.com/lfq7413/tomato/errs"
 	"github.com/lfq7413/tomato/types"
@@ -111,6 +110,118 @@ func Test_parseTypeToPostgresType(t *testing.T) {
 		}
 		if got != tt.want {
 			t.Errorf("%q. parseTypeToPostgresType() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_toPostgresValue(t *testing.T) {
+	type args struct {
+		value interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "1",
+			args: args{value: nil},
+			want: nil,
+		},
+		{
+			name: "2",
+			args: args{value: "hello"},
+			want: "hello",
+		},
+		{
+			name: "3",
+			args: args{
+				value: types.M{"key": "value"},
+			},
+			want: types.M{"key": "value"},
+		},
+		{
+			name: "4",
+			args: args{
+				value: types.M{"__type": "Other"},
+			},
+			want: types.M{"__type": "Other"},
+		},
+		{
+			name: "5",
+			args: args{
+				value: types.M{
+					"__type": "Date",
+					"iso":    "2006-01-02T15:04:05.000Z",
+				},
+			},
+			want: "2006-01-02T15:04:05.000Z",
+		},
+		{
+			name: "6",
+			args: args{
+				value: types.M{
+					"__type": "File",
+					"name":   "image.jpg",
+				},
+			},
+			want: "image.jpg",
+		},
+	}
+	for _, tt := range tests {
+		if got := toPostgresValue(tt.args.value); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. toPostgresValue() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_transformValue(t *testing.T) {
+	type args struct {
+		value interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "1",
+			args: args{value: nil},
+			want: nil,
+		},
+		{
+			name: "2",
+			args: args{value: "hello"},
+			want: "hello",
+		},
+		{
+			name: "3",
+			args: args{
+				value: types.M{"key": "value"},
+			},
+			want: types.M{"key": "value"},
+		},
+		{
+			name: "4",
+			args: args{
+				value: types.M{"__type": "Other"},
+			},
+			want: types.M{"__type": "Other"},
+		},
+		{
+			name: "5",
+			args: args{
+				value: types.M{
+					"__type":   "Pointer",
+					"objectId": "1024",
+				},
+			},
+			want: "1024",
+		},
+	}
+	for _, tt := range tests {
+		if got := transformValue(tt.args.value); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. transformValue() = %v, want %v", tt.name, got, tt.want)
 		}
 	}
 }

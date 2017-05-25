7baba0f8443b7070a36fb3f8d2c3a2c169f1b356
lfq7413 <lfq7413@gmail.com>
2017-05-25 14:53:47 +0800

    重构 send()

--- push/fcm_adapter.go
+++ push/fcm_adapter.go
@@ -22,56 +22,73 @@ func newFCMPush() *fcmPushAdapter {
 }
 
 func (f *fcmPushAdapter) send(body types.M, installations types.S, pushStatus string) []types.M {
-	devices := []types.M{}
-	deviceTokens := []string{}
+	deviceMap := classifyInstallations(installations, f.validPushTypes)
 	results := []types.M{}
 
-	for _, installation := range installations {
-		if dev := utils.M(installation); dev != nil {
-			devices = append(devices, dev)
-			deviceTokens = append(deviceTokens, utils.S(dev["deviceToken"]))
+	loop := func(pushType string) {
+		devices := deviceMap[pushType]
+		if len(devices) == 0 {
+			return
 		}
-	}
+		deviceTokens := []string{}
 
-	status, err := f.sendToRegistrationTokens(deviceTokens, body)
-
-	if err != nil {
 		for _, device := range devices {
-			result := types.M{
-				"device":      device,
-				"transmitted": false,
-				"response":    map[string]string{"error": err.Error()},
-			}
-			results = append(results, result)
+			deviceTokens = append(deviceTokens, utils.S(device["deviceToken"]))
 		}
-		return results
+
+		var status *fcm.FcmResponseStatus
+		var err error
+
+		switch pushType {
+		case "ios", "tvos", "osx":
+			status, err = f.sendToiOSDevices(deviceTokens, body)
+		case "android", "fcm":
+			status, err = f.sendToAndroidDevices(deviceTokens, body)
+		}
+
+		if err != nil {
+			for _, device := range devices {
+				result := types.M{
+					"device":      device,
+					"transmitted": false,
+					"response":    map[string]string{"error": err.Error()},
+				}
+				results = append(results, result)
+			}
+			return
+		}
+
+		multicastID := status.MulticastId
+		pushResults := status.Results
+
+		for index := range deviceTokens {
+			var pushResult map[string]string
+			if pushResults != nil && index < len(pushResults) {
+				pushResult = pushResults[index]
+			} else {
+				pushResult = nil
+			}
+			device := devices[index]
+
+			resolution := types.M{
+				"device":       device,
+				"multicast_id": multicastID,
+				"response":     pushResult,
+			}
+
+			if pushResult == nil || pushResult["error"] != "" {
+				resolution["transmitted"] = false
+			} else {
+				resolution["transmitted"] = true
+			}
+
+			results = append(results, resolution)
+		}
+
 	}
 
-	multicastID := status.MulticastId
-	pushResults := status.Results
-
-	for index := range deviceTokens {
-		var pushResult map[string]string
-		if pushResults != nil && index < len(pushResults) {
-			pushResult = pushResults[index]
-		} else {
-			pushResult = nil
-		}
-		device := devices[index]
-
-		resolution := types.M{
-			"device":       device,
-			"multicast_id": multicastID,
-			"response":     pushResult,
-		}
-
-		if pushResult == nil || pushResult["error"] != "" {
-			resolution["transmitted"] = false
-		} else {
-			resolution["transmitted"] = true
-		}
-
-		results = append(results, resolution)
+	for pushType := range deviceMap {
+		loop(pushType)
 	}
 
 	return results
@@ -81,7 +98,15 @@ func (f *fcmPushAdapter) getValidPushTypes() []string {
 	return f.validPushTypes
 }
 
-func (f *fcmPushAdapter) sendToRegistrationTokens(tokens []string, body types.M) (*fcm.FcmResponseStatus, error) {
+func (f *fcmPushAdapter) sendToiOSDevices(tokens []string, body types.M) (*fcm.FcmResponseStatus, error) {
+	// TODO 转换 body 中的数据到 FCM 支持的格式
+	c := fcm.NewFcmClient(f.serverKey)
+	c.NewFcmRegIdsMsg(tokens, body)
+
+	return c.Send()
+}
+
+func (f *fcmPushAdapter) sendToAndroidDevices(tokens []string, body types.M) (*fcm.FcmResponseStatus, error) {
 	// TODO 转换 body 中的数据到 FCM 支持的格式
 	c := fcm.NewFcmClient(f.serverKey)
 	c.NewFcmRegIdsMsg(tokens, body)
